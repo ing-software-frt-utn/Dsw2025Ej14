4731bc0c5a4c64e6bbce2e6bca7184887e485da5
Fernando Chumba <fernandochumba00gmail.com>
2025-06-10 17:08:08 -0300

    ejercicio 14

---
+++ __Dsw2025Ej14__/__Dsw2025Ej14.Api__/Controllers/ProductsController .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace __Dsw2025Ej14.Api__.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ __Dsw2025Ej14__/__Dsw2025Ej14.Api__/__Domain__/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace __Dsw2025Ej14.Api__.__Domain__
+{
+    public class Product
+    {
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public decimal CurrentUnitPrice { get; set; }
+        public bool IsActive { get; set; }
+
+        public Product(string sku, string name, decimal currentUnitPrice, bool isActive)
+        {
+            Sku = sku;
+            Name = name;
+            CurrentUnitPrice = currentUnitPrice;
+            IsActive = isActive;
+        }
+
+    }
+
+}
