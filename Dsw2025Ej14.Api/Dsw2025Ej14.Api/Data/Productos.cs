7f9100870da9a2efb3564010f8a95cf06781f581
valebugeau <Valentina.Bugeau@alu.frt.utn.edu.ar>
2025-06-10 17:01:53 -0300

    ejercicio de clase 3K2

---
+++ Dsw2025Ej14.Api/Dsw2025Ej14.Api/Data/Productos.cs
@@ -0,0 +1,18 @@
+﻿namespace Dsw2025Ej14.Api.Data
+{
+    public class Productos
+    {
+
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public decimal CurrentUnitPrice { get; set; }
+        public bool IsActive { get; set; }
+
+
+
+
+
+
+
+    }
+}
